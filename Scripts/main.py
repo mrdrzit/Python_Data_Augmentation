69a71589a054e8fb3d8c63b3b2e0e6f8fa718868
Matheus C <uzunaruto.10.mat@gmail.com>
2024-05-21 13:02:34 -0300

    Add TODO to add function to load CSV files into a dataframe
    This commit adds a TODO function to load CSV files into a dataframe. The function takes the CSV files selected by the user and creates a dataframe where the header is the bodyparts row of the CSV, the first row is the name of the image file, and the columns are the x and y coordinates of the bodyparts in the same order as the header. This functionality is implemented using the pandas library.

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,3 +1,21 @@
-import imgaug
+import pandas as pd
+import numpy as np
+import imgaug 
+from tkinter import filedialog
+from pathlib import Path
 
+# Load the image files using tkinter
+images_path = filedialog.askopenfilenames(title='Select the images', filetypes=[('Image Files', '*.jpg *.png *.jpeg')])
+csv_files_path = filedialog.askopenfilenames(title='Select the csv files', filetypes=[('CSV Files', '*.csv')])
 
+# TODO create a function to load the csv files into a dataframe where the header is the bodyparts row of the csv, 
+# the first row is the name of the image file and the columns are the x and y coordinates of the bodyparts in the same order as the header
+# For this you will need the pandas library to read the csv file and create the dataframe
+# Hint to read the csv:
+# https://pandas.pydata.org/docs/reference/api/pandas.read_csv.html
+
+# Hint to create the dataframe:
+# https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.html
+
+# The read dataframe function already imports the data as a dataframe, so it probably easier to create a copy of the dataframe and modify it
+# into a second variable that will be used to store the data in the format described above
